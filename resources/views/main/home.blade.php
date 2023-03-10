525e2d251f7e11fae732d64c31b0d7bbeb6ca0e6
zakaria <zakariaa@gmail.com>
2023-03-10 20:21:00 +0100

    mmm

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -110,7 +110,7 @@ agraf
             <div class="card-item">
               <div class="row">
                 <div class="col-xl-5">
-                  <div class="card-bg" style="background-image: url('{{url("/storage/assets/img/constructions-1.jpg")}}')"></div>
+                  <div class="card-bg" style="background-image: url('{{url("assets('/storage/assets/img/constructions-1.jpg')")}}')"></div>
                 </div>
                 <div class="col-xl-7 d-flex align-items-center">
                   <div class="card-body">
