5d6c0ef4df7cb7bd6cc8a101aad485738ec2b73f
zakaria <zakariaa@gmail.com>
2023-03-30 20:38:10 +0000

    val55

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -398,38 +398,46 @@ Agrtravis - Construction Et Amenagement
       <div class="col-lg-6 img-bg" style="background-image: url('/storage/assets/img/alt-services.jpg');" data-aos="zoom-in" data-aos-delay="100"></div>
 
       <div class="col-lg-5 d-flex flex-column justify-content-center">
-        <h3>Enim quis est voluptatibus aliquid consequatur fugiat</h3>
-        <p>Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus. Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed facere corporis dolores excepturi</p>
+        <h3>Agrtravis - Construction Et Amenagement </h3>
+        <p>Agratravis Entreprise de Construction Et Amenagement En Forme Juridique <b>SARL</b> crée en Febrayer 2022 <br> a Capitale <b>500 000 DH</b> </p>
 
         <div class="icon-box d-flex position-relative fade-in" data-aos="fade-up" data-aos-delay="100">
-          <i class="bi bi-easel flex-shrink-0"></i>
-          <div>
-            <h4><a href="" class="stretched-link">Lorem Ipsum</a></h4>
-            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+          <span class="material-symbols-outlined">
+            home
+            </span>
+          <div class="ms-3">
+            <h4><a href="" class="stretched-link">Adresse</a></h4>
+            <p>AVENUE ABDELKRIM EL KHATTABI IMMEUBLE BEN MOUSSA EL GOUACHE ETAGE 2 </p>
           </div>
         </div><!-- End Icon Box -->
 
         <div class="icon-box d-flex position-relative fade-in" data-aos="fade-up" data-aos-delay="200">
-          <i class="bi bi-patch-check flex-shrink-0"></i>
-          <div>
-            <h4><a href="" class="stretched-link">Nemo Enim</a></h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+          <span class="material-symbols-outlined">
+            call
+            </span>
+          <div class="ms-3">
+            <h4><a href="" class="stretched-link">Téléphone</a></h4>
+            <p>0661577372</p>
           </div>
         </div><!-- End Icon Box -->
 
         <div class="icon-box d-flex position-relative fade-in" data-aos="fade-up" data-aos-delay="300">
-          <i class="bi bi-brightness-high flex-shrink-0"></i>
-          <div>
-            <h4><a href="" class="stretched-link">Dine Pad</a></h4>
-            <p>Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+          <span class="material-symbols-outlined">
+            group
+            </span>
+          <div class="ms-3">
+            <h4><a href="" class="stretched-link">Effectif de l’entreprise</a></h4>
+            <p>12</p>
           </div>
         </div><!-- End Icon Box -->
 
         <div class="icon-box d-flex position-relative fade-in" data-aos="fade-up" data-aos-delay="400">
-          <i class="bi bi-brightness-high flex-shrink-0"></i>
-          <div>
-            <h4><a href="" class="stretched-link">Tride clov</a></h4>
-            <p>Est voluptatem labore deleniti quis a delectus et. Saepe dolorem libero sit non aspernatur odit amet. Et eligendi</p>
+          <span class="material-symbols-outlined">
+            location_on
+            </span>
+          <div class="ms-3">
+            <h4><a href="" class="stretched-link">Ville</a></h4>
+            <p>Fes</p>
           </div>
         </div><!-- End Icon Box -->
 
